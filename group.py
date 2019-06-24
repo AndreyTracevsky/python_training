f2a9509b12f2e53ad5d1bdfde7ccf66881b0456f
AndreyTrcevsky <trac-andrej@ya.ru>
2019-06-24 21:20:46 +0300

    Создали отдельный класс group.py в котором сгруппированно несколько параметров (name, header, footer). Созданный класс Group импортировали в файле test_add_group.

---
+++ group.py
@@ -0,0 +1,8 @@
+# создаем класс в котором будут находиться вспомогательные методы.
+
+class Group:
+
+    def __init__(self, name, header, footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
