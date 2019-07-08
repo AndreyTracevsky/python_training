dc1b89a37232a7c762965d9b3e8df58400f8d8e1
tracevsky <mp1662235>
2019-07-08 15:45:40 +0300

    Пока еще работающий код

--- fixture/application.py
+++ fixture/application.py
@@ -1,37 +1,16 @@
-from selenium import webdriver # импорт перенесен сюда из файла test_add_group.py, т.к драйвер теперь будет запускаться
+from selenium.webdriver.firefox.webdriver import WebDriver # импорт перенесен сюда из файла test_add_group.py, т.к драйвер теперь будет запускаться
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
                                # тут.
 class Application: # В этот класс перенесены все вспомогательные методы из файла test_add_group.py
 
     def __init__(self): # конструктор инициализации
-        self.wd = webdriver.Firefox() # инициализация перенесенная из файла test_add_group.py
-        self.wd.implicitly_wait(30)   #
+        self.wd = WebDriver() # инициализация перенесенная из файла test_add_group.py
+        self.wd.implicitly_wait(60)   #
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self): # вспомогательный метод
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group): # вспомогательный метод
-        wd = self.wd
-        self.open_groups_page() # перенесен сюда из метода "def test_add_group(self)"
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page() # перенесен сюда из метода "def test_add_group(self)"
-
-    def open_groups_page(self): # вспомогательный метод
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+        self.group = GroupHelper(self)
+ 
 
     def open_home_page(self): # вспомогательный метод
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,26 @@
+
+
+
 import pytest # переход на использование фрэймворка pytest
 from model.group import Group # импортируем из файла group.py класс Group
 from fixture.application import Application # импортируем из файла application.py класс Application
 
 
-@pytest.fixture() # пометка для pytest, чтобы он знал что это не просто функция, а ф-ция создающая фикструру.
+@pytest.fixture # пометка для pytest, чтобы он знал что это не просто функция, а ф-ция создающая фикструру.
 def app(request): # функция которая будет инициализироать фикстуру
     fixture = Application() # Создание самой фикстуры (объект типа Application)
     request.addfinalizer(fixture.destroy) #  передается функция которая в определенный момент передается для разрушения фикстуры
     return fixture # Возврат фикстуры
 
+
 def test_add_group(app): # тестовый метод
     #wd = self.wd - Эта строка кода вырезается и вставляется в каждый из вспомогательных методов.
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test", header="test", footer="test"))
+    app.group.create(Group(name="test", header="test", footer="test"))
     app.session.loguot()
 
 def test_add_empty_group(app): # 2-й тестовый метод
     #wd = self.wd - Эта строка кода вырезается и вставляется в каждый из вспомогательных методов.
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.loguot()
